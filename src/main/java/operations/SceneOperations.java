b088381d11846e2f293f88956a27d2eabce095cb
Temus <temus@wir.pl>
2019-05-15 13:14:17 +0200

    v0.0.5

--- src/main/java/operations/SceneOperations.java
+++ src/main/java/operations/SceneOperations.java
@@ -1,4 +1,22 @@
 package operations;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SceneOperations {
+
+    public void changeSceneToPSO(ActionEvent event) throws IOException {
+        Parent EventViewParent = FXMLLoader.load(getClass().getResource("/PSOScene.fxml"));
+        Scene eventScene = new Scene(EventViewParent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(eventScene);
+        window.setTitle("NormalModelEvent panel");
+        window.show();
+    }
 }
