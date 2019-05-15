fc0fdc76a28bfcb4c9f750a428d981559ce80d6a
Temus <temus@wir.pl>
2019-05-15 15:36:13 +0200

    v0.2

--- src/main/java/controller/StartingSceneController.java
+++ src/main/java/controller/StartingSceneController.java
@@ -4,6 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.text.Text;
+import operations.SceneOperations;
+
+import java.io.IOException;
 
 public class StartingSceneController {
     public ComboBox starting_functions_combobox;
@@ -12,10 +15,34 @@ public class StartingSceneController {
     public Text starting_error_text;
     public Text starting_authors_text;
 
+    private static String function;
+    private SceneOperations SO = new SceneOperations();
+
+
     public void saveFunction(ActionEvent event) {
+        String chosenFunction = (String) starting_functions_combobox.getValue();
+
+        if (chosenFunction.equals("Ackley function"))
+            function = "ackley";
+        else if (chosenFunction.equals("Booth function"))
+            function = "booth";
+        else if (chosenFunction.equals("Three-hump camel function"))
+            function = "camel";
+
+
     }
 
     public void startApplication(ActionEvent event) {
+        if (function != null) {
+            try {
+                SO.changeSceneToPSO(event);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            starting_error_text.setVisible(true);
+            starting_error_text.setText("Please, choose a function!");
+        }
     }
 
     public void initialize() {
